2ff6c3e23679430b79dcacc84a2cf845fa1e75ba
Liang(Lizzy) Zhao <59656498+liang0711@users.noreply.github.com>
2023-08-30 13:05:11 -0400

    Update the-report.sql

--- basic-join/the-report.sql
+++ basic-join/the-report.sql
@@ -8,3 +8,13 @@ SELECT IF(GRADES.GRADE>=8, STUDENTS.NAME, NULL),GRADES.GRADE, STUDENTS.MARKS
 FROM GRADES, STUDENTS
 WHERE STUDENTS.MARKS BETWEEN GRADES.MIN_MARK AND GRADES.MAX_MARK
 ORDER BY GRADES.GRADE DESC, STUDENTS.NAME;
+
+https://www.hackerrank.com/challenges/the-report/problem?isFullScreen=true
+
+/*
+
+Notes:
+case statement to selct the specific grade ; 
+when you full join-> each record in student will assgin each grade recorde, such that we can use where clause to select the marks we aim for 
+
+*/
